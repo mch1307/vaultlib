4d95b6ce0fb711bac53d9c6b3b8ea04b0f3c91cf
mch1307 <mch1307@gmail.com>
2018-12-28 23:38:24 +0100

    add sleep after vault start

--- initVaultDev.sh
+++ initVaultDev.sh
@@ -7,22 +7,22 @@ curl -kO https://releases.hashicorp.com/vault/${VAULT_VERSION}/vault_${VAULT_VER
 unzip vault_${VAULT_VERSION}_linux_amd64.zip
 
 #nohup ./vault server -dev -dev-root-token-id ${VAULT_TOKEN}  > /dev/null 2>&1 &
-./vault server -dev -dev-root-token-id ${VAULT_TOKEN}  > /dev/null &
+./vault server -dev -dev-root-token-id ${VAULT_TOKEN}  > /tmp/vaultdev.log &
+sleep 5
 # create KVs
-./vault secrets enable -path=kv_v1/path/ kv > /dev/null 
-#2>&1
-./vault secrets enable -path=kv_v2/path/ kv > /dev/null 
-./vault kv enable-versioning kv_v2/path/ > /dev/null
+./vault secrets enable -path=kv_v1/path/ kv >> /tmp/vaultdev.log
+./vault secrets enable -path=kv_v2/path/ kv >> /tmp/vaultdev.log
+./vault kv enable-versioning kv_v2/path/ >> /tmp/vaultdev.log
 
 # create secrets
-./vault kv put kv_v1/path/my-secret my-v1-secret=my-v1-secret-value > /dev/null
-./vault kv put kv_v2/path/my-secret my-first-secret=my-first-secret-value my-second-secret=my-second-secret-value > /dev/null
+./vault kv put kv_v1/path/my-secret my-v1-secret=my-v1-secret-value >> /tmp/vaultdev.log
+./vault kv put kv_v2/path/my-secret my-first-secret=my-first-secret-value my-second-secret=my-second-secret-value >> /tmp/vaultdev.log
 
 # create policy
-./vault policy write VaultDevAdmin ./VaultPolicy.hcl > /dev/null
+./vault policy write VaultDevAdmin ./VaultPolicy.hcl >> /tmp/vaultdev.log
 
 # create approle
-./vault auth enable approle > /dev/null
-./vault write auth/approle/role/my-role policies=VaultDevAdmin secret_id_ttl=100m token_num_uses=100 token_ttl=100m token_max_ttl=300m secret_id_num_uses=40
+./vault auth enable approle >> /tmp/vaultdev.log
+./vault write auth/approle/role/my-role policies=VaultDevAdmin secret_id_ttl=100m token_num_uses=100 token_ttl=100m token_max_ttl=300m secret_id_num_uses=40 >> /tmp/vaultdev.log
 
 unset VAULT_TOKEN
