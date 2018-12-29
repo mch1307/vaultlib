496b8e244db21febd1edf4dbadff2fffd4e02574
mch1307 <mch1307@gmail.com>
2018-12-29 15:32:16 +0100

    improve coverage

--- integration_test.go
+++ integration_test.go
@@ -5,17 +5,23 @@ import (
 	"log"
 	"os"
 	"os/exec"
-	"reflect"
 	"testing"
 	"time"
 )
 
 var vaultRoleID, vaultSecretID string
 
+func TestMain(m *testing.M) {
+	fmt.Println("TestMain: Preparing Vault server")
+	prepareVault()
+	ret := m.Run()
+	os.Exit(ret)
+}
+
 func prepareVault() {
 	go startVault()
-	// wait 30 seconds vault
-	time.Sleep(30 * time.Second)
+	// wait 20 seconds vault
+	time.Sleep(20 * time.Second)
 	cmd := exec.Command("./vault", "read", "-field=role_id", "auth/approle/role/my-role/role-id")
 	cmd.Env = os.Environ()
 	cmd.Env = append(cmd.Env, "VAULT_TOKEN=my-dev-root-vault-token")
@@ -48,46 +54,4 @@ func startVault() {
 	}
 	_ = cmd.Wait()
 
-}
-func TestMain(m *testing.M) {
-	fmt.Println("TestMain: Preparing Vault server")
-	prepareVault()
-	ret := m.Run()
-	os.Exit(ret)
-}
-
-func TestVaultClient_GetVaultSecret(t *testing.T) {
-	_ = os.Unsetenv("VAULT_TOKEN")
-	conf := NewConfig()
-	conf.AppRoleCredentials.RoleID = vaultRoleID
-	conf.AppRoleCredentials.SecretID = vaultSecretID
-	vc, err := NewClient(conf)
-	if err != nil {
-		t.Errorf("Failed to get vault cli %v", err)
-	}
-
-	tests := []struct {
-		name    string
-		cli     *VaultClient
-		path    string
-		wantKv  map[string]string
-		wantErr bool
-	}{
-		{"kvv1", vc, "kv_v1/path/my-secret", map[string]string{"my-v1-secret": "my-v1-secret-value"}, false},
-		{"kvv2", vc, "kv_v2/path/my-secret", map[string]string{"my-first-secret": "my-first-secret-value",
-			"my-second-secret": "my-second-secret-value"}, false},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			c := tt.cli
-			gotKv, err := c.GetVaultSecret(tt.path)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("VaultClient.GetVaultSecret() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(gotKv, tt.wantKv) {
-				t.Errorf("VaultClient.GetVaultSecret() = %v, want %v", gotKv, tt.wantKv)
-			}
-		})
-	}
 }
--- vault_test.go
+++ vault_test.go
@@ -1,6 +1,10 @@
 package vaultlib
 
 import (
+	"net/http"
+	"net/url"
+	"os"
+	"reflect"
 	"testing"
 )
 
@@ -14,6 +18,12 @@ func TestVaultClient_getKVInfo(t *testing.T) {
 	_ = conf.setAppRole(cred)
 	badReqConf := NewConfig()
 	badReqConf.Address = "https://localhost:8200"
+	noCred := AppRoleCredentials{
+		RoleID:   "",
+		SecretID: "",
+	}
+	noCredConf := NewConfig()
+	noCredConf.AppRoleCredentials = &noCred
 
 	type fields struct {
 		Config *Config
@@ -33,6 +43,7 @@ func TestVaultClient_getKVInfo(t *testing.T) {
 		{"foundV2", fields{conf}, args{"kv_v2/path/my-secret"}, "2", "kv_v2/path/", false},
 		{"notFound", fields{conf}, args{"notExist/my-secret"}, "", "", true},
 		{"badRequest", fields{badReqConf}, args{"notExist/my-secret"}, "", "", true},
+		{"NoCred", fields{noCredConf}, args{"notExist/my-secret"}, "", "", true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -47,6 +58,78 @@ func TestVaultClient_getKVInfo(t *testing.T) {
 			}
 			if gotName != tt.wantName {
 				t.Errorf("VaultClient.getKVInfo() gotName = %v, want %v", gotName, tt.wantName)
+			}
+		})
+	}
+}
+
+func TestVaultClient_setTokenFromAppRole(t *testing.T) {
+	rightURL, _ := url.Parse("http://localhost:8200")
+	badURL, _ := url.Parse("https://localhost:8200")
+	conf := NewConfig()
+	htCli := new(http.Client)
+	type fields struct {
+		Address    *url.URL
+		HTTPClient *http.Client
+		Config     *Config
+		Token      string
+		Status     string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantErr bool
+	}{
+		{"tokenKO", fields{rightURL, htCli, conf, "bad-token", ""}, true},
+		{"badUrl", fields{badURL, htCli, conf, "bad-token", ""}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &VaultClient{
+				Address:    tt.fields.Address,
+				HTTPClient: tt.fields.HTTPClient,
+				Config:     tt.fields.Config,
+				Token:      tt.fields.Token,
+				Status:     tt.fields.Status,
+			}
+			if err := c.setTokenFromAppRole(); (err != nil) != tt.wantErr {
+				t.Errorf("VaultClient.setTokenFromAppRole() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestVaultClient_GetVaultSecret(t *testing.T) {
+	_ = os.Unsetenv("VAULT_TOKEN")
+	conf := NewConfig()
+	conf.AppRoleCredentials.RoleID = vaultRoleID
+	conf.AppRoleCredentials.SecretID = vaultSecretID
+	vc, err := NewClient(conf)
+	if err != nil {
+		t.Errorf("Failed to get vault cli %v", err)
+	}
+
+	tests := []struct {
+		name    string
+		cli     *VaultClient
+		path    string
+		wantKv  map[string]string
+		wantErr bool
+	}{
+		{"kvv1", vc, "kv_v1/path/my-secret", map[string]string{"my-v1-secret": "my-v1-secret-value"}, false},
+		{"kvv2", vc, "kv_v2/path/my-secret", map[string]string{"my-first-secret": "my-first-secret-value",
+			"my-second-secret": "my-second-secret-value"}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := tt.cli
+			gotKv, err := c.GetVaultSecret(tt.path)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("VaultClient.GetVaultSecret() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(gotKv, tt.wantKv) {
+				t.Errorf("VaultClient.GetVaultSecret() = %v, want %v", gotKv, tt.wantKv)
 			}
 		})
 	}
