52bcd5e69904180064117067312cc28fa700070d
mch1307 <mch1307@gmail.com>
2018-12-29 10:47:54 +0100

    improve coverage

--- vault_test.go
+++ vault_test.go
@@ -1,35 +1,53 @@
 package vaultlib
 
 import (
-	"os"
 	"testing"
 )
 
-func TestVaultClient_getKVVersion(t *testing.T) {
+func TestVaultClient_getKVInfo(t *testing.T) {
+	conf := NewConfig()
+	conf.Address = "http://localhost:8200"
+	cred := AppRoleCredentials{
+		RoleID:   vaultRoleID,
+		SecretID: vaultSecretID,
+	}
+	_ = conf.setAppRole(cred)
+	badReqConf := NewConfig()
+	badReqConf.Address = "https://localhost:8200"
 
-	t.Run("test", func(t *testing.T) {
-		os.Setenv("VAULT_ADDR", "http://localhost:8200")
-		// Create a new config. Reads env variables, fallback to default value if needed
-		conf := NewConfig()
-		cred := AppRoleCredentials{
-			RoleID:   vaultRoleID,
-			SecretID: vaultSecretID,
-		}
-		_ = conf.setAppRole(cred)
-		cli, _ := NewClient(conf)
-
-		err := cli.setTokenFromAppRole()
-		if err != nil {
-			t.Errorf("error with app role auth: %v", err)
-		}
-
-		gotVersion, _, err := cli.getKVInfo("kv_v1/path/")
-		if err != nil {
-			t.Errorf("Err:  %v", err)
-		}
-		if gotVersion != "1" {
-			t.Errorf("VaultClient.getKVVersion() = %v, want %v", gotVersion, "1")
-		}
-	})
-
+	type fields struct {
+		Config *Config
+	}
+	type args struct {
+		path string
+	}
+	tests := []struct {
+		name        string
+		fields      fields
+		args        args
+		wantVersion string
+		wantName    string
+		wantErr     bool
+	}{
+		{"foundV1", fields{conf}, args{"kv_v1/path/my-secret"}, "1", "kv_v1/path/", false},
+		{"foundV2", fields{conf}, args{"kv_v2/path/my-secret"}, "2", "kv_v2/path/", false},
+		{"notFound", fields{conf}, args{"notExist/my-secret"}, "", "", true},
+		{"badRequest", fields{badReqConf}, args{"notExist/my-secret"}, "", "", true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c, _ := NewClient(tt.fields.Config)
+			gotVersion, gotName, err := c.getKVInfo(tt.args.path)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("VaultClient.getKVInfo() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if gotVersion != tt.wantVersion {
+				t.Errorf("VaultClient.getKVInfo() gotVersion = %v, want %v", gotVersion, tt.wantVersion)
+			}
+			if gotName != tt.wantName {
+				t.Errorf("VaultClient.getKVInfo() gotName = %v, want %v", gotName, tt.wantName)
+			}
+		})
+	}
 }
