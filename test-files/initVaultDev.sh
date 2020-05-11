61260a6f8972b91ff320a40b80436d65b24badf0
dericmiller <dericmiller@gmail.com>
2020-05-11 15:38:55 -0400

    🐛 Make integration tests multiplatform
    - Integration test setup: update default Vault version to 1.4.1
    
    PR #1
    [close ch1055]

--- test-files/initVaultDev.sh
+++ test-files/initVaultDev.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 export VAULT_ADDR=http://localhost:8200
 export VAULT_TOKEN=my-dev-root-vault-token
-export VAULT_VERSION=${1:-1.0.1}
+export VAULT_VERSION=${1:-1.4.1}
+
+case "$(uname -s)" in
+  Darwin*)
+    OS="darwin_amd64"
+    ;;
+  MINGW64*)
+    OS="windows_amd64"
+    ;;
+  *)
+    OS="linux_amd64"
+    ;;
+esac
 
 CURRENT_VAULT=`./vault version | cut -d'v' -f2 | cut -d' ' -f1`
 if [ "$CURRENT_VAULT" != "$VAULT_VERSION" ]; then
     rm -rf ./vault
-    curl -kO https://releases.hashicorp.com/vault/${VAULT_VERSION}/vault_${VAULT_VERSION}_linux_amd64.zip
-    unzip vault_${VAULT_VERSION}_linux_amd64.zip
+    curl -kO https://releases.hashicorp.com/vault/${VAULT_VERSION}/vault_${VAULT_VERSION}_${OS}.zip
+    unzip vault_${VAULT_VERSION}_${OS}.zip
 fi
 
 ./vault server -dev -dev-root-token-id ${VAULT_TOKEN}  > /tmp/vaultdev.log &
