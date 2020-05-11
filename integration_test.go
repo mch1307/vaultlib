159314749babcd684299895a566e4b12b817c219
dericmiller <dericmiller@gmail.com>
2020-05-11 18:21:49 -0400

    🐛 Update handling of flags for Go 1.13+
    Go 1.13 introduces a Testing.Init function for handling test flags.
    
    https://tip.golang.org/doc/go1.13#testing
    
    This has the unfortunate consequence of panicking if flags are parsed
    before testing.Init is called (for instance, in init). We therefore
    update the code to parse flags in the Testing Main override such that
    tests may be run in Go 1.11-1.14.
    
    Related:
    https://github.com/golang/go/issues/31859
    
    Co-authored-by: Andrew Pinkham <code@andrewsforge.com>
    
    PR #3
    [close ch1056]

--- integration_test.go
+++ integration_test.go
@@ -13,15 +13,18 @@ var vaultRoleID, vaultSecretID, noKVRoleID, noKVSecretID string
 
 var vaultVersion string
 
-func init() {
-	flag.StringVar(&vaultVersion, "vaultVersion", "1.4.1", "provide vault version to be tested against")
-	flag.Parse()
-}
 func TestMain(m *testing.M) {
-
+	flag.StringVar(
+		&vaultVersion,
+		"vaultVersion",
+		"1.4.1",
+		"provide vault version to be tested against",
+	)
+	flag.Parse()
 	fmt.Println("Testing with Vault version", vaultVersion)
 	fmt.Println("TestMain: Preparing Vault server")
 	prepareVault()
+	fmt.Println("TestMain: Vault Prepared; Running Tests")
 	ret := m.Run()
 	os.Exit(ret)
 }
